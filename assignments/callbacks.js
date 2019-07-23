8072121f3cfd87162f0aeac5369be3e4bd7b3be5
Heorhii Siburov <g.siburov@gmail.com>
2019-07-23 14:31:05 -0700

    callbacks.js - mvp done

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -3,7 +3,7 @@
 const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 
-/* 
+
 
   //Given this problem: 
   
@@ -23,29 +23,65 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
     console.log(first)
   });
 
-*/
-
+// =========
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  return cb(arr.length);
 }
+
+getLength(items, function(length) {
+  console.log(length)
+});
+
+// =========
 
 function last(arr, cb) {
   // last passes the last item of the array into the callback.
+  return cb(arr[arr.length - 1]);
 }
+last(items, function(lastItem) {
+  console.log(lastItem);
+});
+
+// =========
 
 function sumNums(x, y, cb) {
   // sumNums adds two numbers (x, y) and passes the result to the callback.
+  return cb(x + y);
 }
+sumNums(5, 7, function(sum) {
+  console.log(sum);
+});
+
+// =========
 
 function multiplyNums(x, y, cb) {
   // multiplyNums multiplies two numbers and passes the result to the callback.
+  return cb(x * y);
 }
+multiplyNums(676, 234234, function(mult) {
+  console.log(mult);
+});
+
+// =========
 
 function contains(item, list, cb) {
   // contains checks if an item is present inside of the given array/list.
   // Pass true to the callback if it is, otherwise pass false.
+  if (list.includes(item)) {
+    return cb(true);
+  }
+  return cb(false);
 }
+
+function trueFalse(boolean) {
+  console.log(boolean);
+}
+contains("Dog", items, trueFalse);
+contains("Cat", items, trueFalse);
+contains("Notebook", items, trueFalse);
+
 
 /* STRETCH PROBLEM */
 
