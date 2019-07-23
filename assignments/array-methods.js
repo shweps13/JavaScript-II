d13165aeb2f1e1fad5e8c6225edc4f6afbbb06ad
Heorhii Siburov <g.siburov@gmail.com>
2019-07-23 16:33:21 -0700

    array - fixed

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -55,9 +55,8 @@ const runners = [{"id":1,"first_name":"Charmain","last_name":"Seiler","email":"c
 
 // ==== Challenge 1: Use .forEach() ====
 // The event director needs both the first and last names of each runner for their running bibs.  Combine both the first and last names into a new array called fullName. 
-let fullName = runners.map(function(firstLast){
-    return {first_name: firstLast.first_name, last_name: firstLast.last_name}
-  });
+let fullName = [];
+    runners.forEach(runner => fullName.push(runner.first_name + ' ' + runner.last_name));
 console.log(fullName);
 
 // ==== Challenge 2: Use .map() ====
@@ -88,8 +87,22 @@ let ticketPriceTotal = runners.reduce((acc, cost) => {
 // ==== Challenge 5: Be Creative ====
 // Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  Try to create and then solve 3 unique problems using one or many of the array methods listed above.
 
-// Problem 1
+// Problem 1: Email list
+let runEmails = runners.map((runner) => {
+    return runner.email;
+});
+console.log(runEmails);
 
-// Problem 2
+// Problem 2: List names and companies
+let companyName = [];
+    runners.forEach(runner => companyName.push(runner.first_name + ' from ' + runner.company_name));
+console.log(companyName);
 
-// Problem 3
+// Problem 3: List of guys with small shirths
+let = smallShirts = runners.filter(function(sizeM) {
+    if (sizeM.shirt_size === 'M') {
+      return true;
+    }
+    return false;
+});
+console.log(smallShirts);
